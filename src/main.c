a337014b7f15d5cf58d94ab248be8a1a1cce8b51
AHDiniz <AHDiniz@github.com>
2018-11-08 21:21:47 -0200

    Repository setup

---
+++ src/main.c
@@ -0,0 +1,17 @@
+/**
+ * ED2-2018-2-TC3: Password Cracking problem
+ * 
+ * Authors: Alan Herculano Diniz & Rafael Belmock Pedruzzi
+ * 
+ **********************************************************
+ *
+ * main.c: program's entry point
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
+
